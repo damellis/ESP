d5168ea420fae82b0a27fbccdf4e9ec386f7023d
David A. Mellis <dam@mellis.org>
2015-11-13 15:42:06 -0800

    Initial commit.
    Example Processing program that extracts the mean and standard
    deviation of incoming sensor data (by some time window), allows
    association of class labels (0 to 9) with data from the latest time
    window, recognizes the class of the closest neighbor of the latest time
    window, and generates a data file for an Arduino program that does
    similar recognition.
    
    This is a proof-of-concept example, with canned features and
    classification algorithm.

---
+++ Arduino/ADXL335/ADXL335.ino
@@ -0,0 +1,22 @@
+int vinpin = A0;
+int voutpin = A1;
+int gndpin = A2;
+int zpin = A3;
+int ypin = A4;
+int xpin = A5;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(vinpin, OUTPUT); digitalWrite(vinpin, HIGH);
+  pinMode(gndpin, OUTPUT); digitalWrite(gndpin, LOW);
+  pinMode(voutpin, INPUT);
+  pinMode(xpin, INPUT);
+  pinMode(ypin, INPUT);
+  pinMode(zpin, INPUT);
+}
+
+void loop() {
+  Serial.print(analogRead(xpin)); Serial.print("\t");
+  Serial.print(analogRead(ypin)); Serial.print("\t");
+  Serial.print(analogRead(zpin)); Serial.println();
+}
