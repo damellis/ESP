584fbc6210d43bc7232e0caf2b6e25005542d30c
David A. Mellis <dam@mellis.org>
2016-02-08 15:46:45 -0800

    Add accelerometer example.
    Uses the ADXL335 example in the Arduino folder.

---
+++ Xcode/Audio/src/user_adxl335_accelerometer.h
@@ -0,0 +1,18 @@
+#include "GRT/GRT.h"
+#include "istream.h"
+
+using namespace GRT;
+
+ASCIISerialStream stream(115200, 3);
+GestureRecognitionPipeline pipeline;
+
+void setup()
+{
+    stream.useUSBPort(0);
+    useStream(stream);
+    
+    pipeline.addFeatureExtractionModule(TimeDomainFeatures(10, 1, 3, false, true, true, false, false));
+    pipeline.setClassifier(ANBC(false, true));
+    pipeline.addPostProcessingModule(ClassLabelFilter(15, 20));
+    usePipeline(pipeline);
+}
