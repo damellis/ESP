fd34c8cb7e0a5edf5ffee2b2eb81cd14dfe4ec29
Ben Zhang <benzh@cs.berkeley.edu>
2016-02-10 23:34:00 -0800

    Removed unused Xcode project GUI

--- Xcode/GUI/src/user.h
+++
@@ -1,7 +0,0 @@
-#define DIM 3
-
-void setupPipeline()
-{
-	pipeline.addFeatureExtractionModule(TimeDomainFeatures(100, 1, DIM, false, true, true, false, false));
-	pipeline.setClassifier(KNN(1));
-}
