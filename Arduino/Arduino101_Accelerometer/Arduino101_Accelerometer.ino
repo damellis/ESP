506fae7cc380543e9c0d4c31010d97dd78cd8d9c
Ben Zhang <benzh@cs.berkeley.edu>
2016-04-01 17:57:20 -0700

    Use Arduino101 CurieIMU new APIs.

--- Arduino/Arduino101_Accelerometer/Arduino101_Accelerometer.ino
+++ Arduino/Arduino101_Accelerometer/Arduino101_Accelerometer.ino
@@ -1,22 +1,22 @@
-#include "CurieImu.h"
+#include "CurieIMU.h"
 
-int16_t ax, ay, az;
+int ax, ay, az;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  CurieImu.initialize();
+  CurieIMU.begin();
 
-  if (!CurieImu.testConnection()) {
+  if (!CurieIMU.testConnection()) {
     Serial.println("CurieImu connection failed");
   }
 
-  CurieImu.setFullScaleAccelRange(BMI160_ACCEL_RANGE_8G);
+  CurieIMU.setAccelerometerRange(8);
 }
 
 void loop() {
-  CurieImu.getAcceleration(&ax, &ay, &az);
+  CurieIMU.readAccelerometer(ax, ay, az);
   Serial.print(ax);
   Serial.print("\t");
   Serial.print(ay);
