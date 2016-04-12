259c6d5ac548fe04cf23a8699c072ea4e1287d2b
David A. Mellis <dam@mellis.org>
2016-04-12 13:59:10 -0700

    Add calibration to accelerometer poses example.

--- Xcode/SmartSensors/src/user_accelerometer_poses.h
+++ Xcode/SmartSensors/src/user_accelerometer_poses.h
@@ -2,6 +2,7 @@
 
 ASCIISerialStream stream(0, 9600, 3);
 GestureRecognitionPipeline pipeline;
+Calibrator calibrator;
 TcpOStream oStream("localhost", 5204, 3, "l", "r", " ");
 
 float analogReadToVoltage(float input)
@@ -19,19 +20,72 @@ float normalizeArduino101(float input)
     return input / 4096;
 }
 
+// accelerometer characteristics to be calculated from calibration
+double zeroG, oneG;
+
+vector<double> processAccelerometerData(vector<double> input)
+{
+    if (input.size() < 3) return input;
+
+    vector<double> output(input.size());
+
+    output[0] = (input[0] - zeroG) / (oneG - zeroG);
+    output[1] = (input[1] - zeroG) / (oneG - zeroG);
+    output[2] = (input[2] - zeroG) / (oneG - zeroG);
+
+    return output;
+}
+
+void restingDataCollected(const MatrixDouble& data)
+{
+    std::vector<float> mean(3, 0.0);
+
+    for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < data.getNumRows(); i++)
+        {
+            mean[j] += data[i][j];
+        }
+        mean[j] /= data.getNumRows();
+    }
+
+    // TODO: give warning if mean[0] (X acceleration) and mean[1] (Y accleration) are different.
+    zeroG = (mean[0] + mean[1]) / 2; // take average of X and Y acceleration as the zero G value
+    oneG = mean[2]; // use Z acceleration as one G value (due to gravity)
+}
+
+int timeout = 500;
+double null_rej = 5.0;
+
 void setup()
 {
     stream.useNormalizer(normalizeADXL335);
     stream.setLabelsForAllDimensions({"x", "y", "z"});
     useStream(stream);
+
+    calibrator.setCalibrateFunction(processAccelerometerData);
+    // The elaborate version is:
+    // CalibrateProcess cp("Resting", "Rest accelerometer on flat surface, w/ z-axis vertical.", restingDataCollected);
+    // calibrator.addCalibrateProcess(cp);
+    calibrator.addCalibrateProcess("Resting", "Rest accelerometer on flat surface, w/ z-axis vertical.", restingDataCollected);
+    useCalibrator(calibrator);
     
     pipeline.addFeatureExtractionModule(TimeDomainFeatures(10, 1, 3, false, true, true, false, false));
-    pipeline.setClassifier(ANBC(false, true, 10.0)); // use scaling, use null rejection, null rejection parameter
+    pipeline.setClassifier(ANBC(false, true, null_rej)); // use scaling, use null rejection, null rejection parameter
     // null rejection parameter is multiplied by the standard deviation to determine
     // the rejection threshold. the higher the number, the looser the filter; the
     // lower the number, the tighter the filter.
 
+    pipeline.addPostProcessingModule(ClassLabelTimeoutFilter(timeout));
     usePipeline(pipeline);
     
+    registerTuneable(timeout, 10, 1000, "Timeout",
+                     "The longer, more filtering effect on the result");
+    registerTuneable(null_rej, 0.1, 20.0,
+                     "null rejection",
+                     "null rejection parameter is multiplied by the standard deviation"
+                     " to determine the rejection threshold. "
+                     "The higher the number, the looser the filter; "
+                     "the lower the number, the tighter the filter.");
+
     useOStream(oStream);
 }
