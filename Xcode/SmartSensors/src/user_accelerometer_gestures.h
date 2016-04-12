b0ada29d9a59850396d67021c0d2570e0b9305fa
David A. Mellis <dam@mellis.org>
2016-04-12 14:00:00 -0700

    Add tunable parameters to accelerometer gestures example.

--- Xcode/SmartSensors/src/user_accelerometer_gestures.h
+++ Xcode/SmartSensors/src/user_accelerometer_gestures.h
@@ -19,13 +19,16 @@ float normalizeArduino101(float input)
     return input / 4096;
 }
 
+int timeout = 500;
+double threshold = 0.4;
+
 void setup()
 {
     stream.useNormalizer(normalizeADXL335);
     stream.setLabelsForAllDimensions({"x", "y", "z"});
     useStream(stream);
     
-    pipeline.setClassifier(DTW(false, true, 0.35)); // use scaling, use null rejection, null rejection parameter
+    pipeline.setClassifier(DTW(false, true, threshold)); // don't use scaling, use null rejection, null rejection parameter
     // null rejection parameter is multiplied by the standard deviation to determine
     // the rejection threshold. the higher the number, the looser the filter; the
     // lower the number, the tighter the filter.
@@ -34,9 +37,18 @@ void setup()
     // properly when replaying saved sensor data (which is stored without
     // timestamps). Instead, filter by number of samples using a
     // ClassLabelFilter.
-    //pipeline.addPostProcessingModule(ClassLabelTimeoutFilter(500));
-    pipeline.addPostProcessingModule(ClassLabelFilter(1, 25));
+    pipeline.addPostProcessingModule(ClassLabelTimeoutFilter(timeout));
+    //pipeline.addPostProcessingModule(ClassLabelFilter(1, 25));
     usePipeline(pipeline);
     
+    registerTuneable(threshold, 0.1, 3.0,
+        "Distance Threshold",
+        "How similar a live gesture needs to be to a training sample. "
+        "The lower the number, the more similar it needs to be.");
+    registerTuneable(timeout, 10, 1000,
+        "Delay Between Gestures",
+        "How long (in milliseconds) to wait after recognizing a "
+        "gesture before recognizing another one.");
+
     useOStream(oStream);
 }
