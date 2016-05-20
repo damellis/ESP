5138a3ee7f83f22ab49f3448515cfb6ffac95837
David A. Mellis <dam@mellis.org>
2016-05-19 18:52:23 -0700

    Fix to ADXL335 Arduino code comments.

--- Arduino/ADXL335/ADXL335.ino
+++ Arduino/ADXL335/ADXL335.ino
@@ -2,19 +2,24 @@ int zpin = A3;
 int ypin = A4;
 int xpin = A5;
 
-// Uncomment the following lines if you're using an ADXL335 on an
-// Adafruit breakout board (https://www.adafruit.com/products/163)
-// and want to plug it directly into (and power it from) the analog
-// input pins of your Arduino board.
-//int vinpin = A0;
-//int voutpin = A1;
-//int gndpin = A2;
+// These are only used if you're plugging the ADXL335 (on the
+// Adafruit breakout board) directly into the analog input pins
+// of your Arduino. See comment below.
+int vinpin = A0;
+int voutpin = A1;
+int gndpin = A2;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(vinpin, OUTPUT); digitalWrite(vinpin, HIGH);
-  pinMode(gndpin, OUTPUT); digitalWrite(gndpin, LOW);
-  pinMode(voutpin, INPUT);
+
+  // Uncomment the following lines if you're using an ADXL335 on an
+  // Adafruit breakout board (https://www.adafruit.com/products/163)
+  // and want to plug it directly into (and power it from) the analog
+  // input pins of your Arduino board.
+//  pinMode(vinpin, OUTPUT); digitalWrite(vinpin, HIGH);
+//  pinMode(gndpin, OUTPUT); digitalWrite(gndpin, LOW);
+//  pinMode(voutpin, INPUT);
+  
   pinMode(xpin, INPUT);
   pinMode(ypin, INPUT);
   pinMode(zpin, INPUT);
