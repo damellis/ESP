edc85bd1cd77eb1eb5c13bbffce2414f1e6552bb
David A. Mellis <dam@mellis.org>
2016-05-23 10:12:04 -0700

    Update example file extensions (.h to .cpp).

--- Xcode/ESP/src/user.cpp
+++ Xcode/ESP/src/user.cpp
@@ -3,8 +3,8 @@
 // This is a hack.
 // Otherwise, we will need to create a target for each user application.
 #include "examples/user_audio_beat.cpp"
-//#include "examples/user_color_sensor.h"
-//#include "examples/user_accelerometer_gestures.h"
-//#include "examples/user_accelerometer_poses.h"
-//#include "examples/user_capacitive_sensing.h"
-//#include "examples/user_sudden_motion.h"
+//#include "examples/user_color_sensor.cpp"
+//#include "examples/user_accelerometer_gestures.cpp"
+//#include "examples/user_accelerometer_poses.cpp"
+//#include "examples/user_capacitive_sensing.cpp"
+//#include "examples/user_sudden_motion.cpp"
