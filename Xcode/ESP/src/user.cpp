bfb71c474ad250c3edf11208bddb3a17b53cf8c6
David A. Mellis <dam@mellis.org>
2016-06-23 11:22:34 -0700

    Default to audio example.

--- Xcode/ESP/src/user.cpp
+++ Xcode/ESP/src/user.cpp
@@ -2,12 +2,12 @@
 
 // This is a hack.
 // Otherwise, we will need to create a target for each user application.
-//#include "examples/user_audio_beat.cpp"
+#include "examples/user_audio_beat.cpp"
 //#include "examples/user_audio_beat_detection.cpp"
 //#include "examples/user_color_sensor.cpp"
 //#include "examples/user_accelerometer_gestures.cpp"
 //#include "examples/user_accelerometer_poses.cpp"
-#include "examples/user_accelerometer_walk_detection.cpp"
+//#include "examples/user_accelerometer_walk_detection.cpp"
 //#include "examples/user_capacitive_sensing.cpp"
 //#include "examples/user_speech_detection.cpp"
 //#include "examples/user_sudden_motion.cpp"
