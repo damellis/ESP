f39017af3d8060960d0059ba81834e7870d75046
David A. Mellis <dam@mellis.org>
2016-06-24 10:33:03 -0700

    Update calibration in accelerometer poses.

--- Xcode/ESP/src/examples/user_accelerometer_poses.cpp
+++ Xcode/ESP/src/examples/user_accelerometer_poses.cpp
@@ -8,60 +8,35 @@ GestureRecognitionPipeline pipeline;
 Calibrator calibrator;
 TcpOStream oStream("localhost", 5204, 3, "l", "r", " ");
 
-float analogReadToVoltage(float input)
+double zeroG = 0, oneG = 0;
+
+double processAccelerometerData(double input)
 {
-    return input / 1024.0 * 5.0;
-}
-
-float normalizeADXL335(float input)
-{
-    return (analogReadToVoltage(input) - 1.66) / 0.333;
-}
-
-float normalizeArduino101(float input)
-{
-    return input / 4096;
-}
-
-// accelerometer characteristics to be calculated from calibration
-double zeroG, oneG;
-double calibration_tolerance = 0.1; // 0.1 g difference
-
-vector<double> processAccelerometerData(vector<double> input)
-{
-    if (input.size() < 3) return input;
-
-    vector<double> output(input.size());
-
-    output[0] = (input[0] - zeroG) / (oneG - zeroG);
-    output[1] = (input[1] - zeroG) / (oneG - zeroG);
-    output[2] = (input[2] - zeroG) / (oneG - zeroG);
-
-    return output;
+    return (input - zeroG) / (oneG - zeroG);
 }
 
 CalibrateResult restingDataCollected(const MatrixDouble& data)
 {
-    std::vector<float> mean(3, 0.0);
+    // take average of X and Y acceleration as the zero G value
+    zeroG = (data.getMean()[0] + data.getMean()[1]) / 2;
+    oneG = data.getMean()[2]; // use Z acceleration as one G value
+    
+    double range = abs(oneG - zeroG);
+    vector<double> stddev = data.getStdDev();
+    
+    if (stddev[0] / range > 0.05 ||
+        stddev[1] / range > 0.05 ||
+        stddev[2] / range > 0.05)
+        return CalibrateResult(CalibrateResult::WARNING,
+            "Accelerometer seemed to be moving; consider recollecting the "
+            "calibration sample.");
+    
+    if (abs(data.getMean()[0] - data.getMean()[1]) / range > 0.1)
+        return CalibrateResult(CalibrateResult::WARNING,
+            "X and Y axes differ by " + std::to_string(
+            abs(data.getMean()[0] - data.getMean()[1]) / range * 100) +
+            " percent. Check that accelerometer is flat.");
 
-    for (int j = 0; j < 3; j++) {
-        for (int i = 0; i < data.getNumRows(); i++)
-        {
-            mean[j] += data[i][j];
-        }
-        mean[j] /= data.getNumRows();
-    }
-
-    // TODO: give warning if mean[0] (X acceleration) and mean[1] (Y
-    // accleration) are different.
-    if (std::abs(mean[0] - mean[1]) > calibration_tolerance) {
-        return CalibrateResult(CalibrateResult::FAILURE,
-                               "Please rest the accelerometer on a flat surface"
-                               " so that x- and y-axis will be the same");
-    }
-
-    zeroG = (mean[0] + mean[1]) / 2; // take average of X and Y acceleration as the zero G value
-    oneG = mean[2]; // use Z acceleration as one G value (due to gravity)
     return CalibrateResult::SUCCESS;
 }
 
@@ -72,17 +47,14 @@ int timeout = 100;
 
 void setup()
 {
-    stream.useNormalizer(normalizeADXL335);
     stream.setLabelsForAllDimensions({"x", "y", "z"});
     useInputStream(stream);
     useOutputStream(oStream);
     //useStream(stream);
 
     calibrator.setCalibrateFunction(processAccelerometerData);
-    // The elaborate version is:
-    // CalibrateProcess cp("Resting", "Rest accelerometer on flat surface, w/ z-axis vertical.", restingDataCollected);
-    // calibrator.addCalibrateProcess(cp);
-    calibrator.addCalibrateProcess("Resting", "Rest accelerometer on flat surface, w/ z-axis vertical.", restingDataCollected);
+    calibrator.addCalibrateProcess("Resting",
+        "Rest accelerometer on flat surface.", restingDataCollected);
     useCalibrator(calibrator);
 
     pipeline.addFeatureExtractionModule(TimeDomainFeatures(10, 1, 3, false, true, true, false, false));
