362fb040f62cb42e45448e93210cb69a948c86a4
David A. Mellis <dam@mellis.org>
2016-07-07 13:41:51 -0700

    Auto-format / re-indent Touche example.

--- Arduino/Touche/Touche.ino
+++ Arduino/Touche/Touche.ino
@@ -1,7 +1,7 @@
-   
 
-   
-   //****************************************************************************************
+
+
+//****************************************************************************************
 // Illutron take on Disney style capacitive touch sensor using only passives and Arduino
 // Dzl 2012
 //****************************************************************************************
@@ -35,26 +35,26 @@ float results[N];            //-Filtered result buffer
 float freq[N];            //-Filtered result buffer
 int sizeOfArray = N;
 
- 
-   
-   
+
+
+
 
 void setup()
 {
-  
-  
-  TCCR1A=0b10000010;        //-Set up frequency generator
-  TCCR1B=0b00011001;        //-+
-  ICR1=110;
-  OCR1A=55;
 
-  pinMode(9,OUTPUT);        //-Signal generator pin
-  pinMode(8,OUTPUT);        //-Sync (test) pin
+
+  TCCR1A = 0b10000010;      //-Set up frequency generator
+  TCCR1B = 0b00011001;      //-+
+  ICR1 = 110;
+  OCR1A = 55;
+
+  pinMode(9, OUTPUT);       //-Signal generator pin
+  pinMode(8, OUTPUT);       //-Sync (test) pin
 
   Serial.begin(115200);
 
-  for(int i=0;i<N;i++)      //-Preset results
-    results[i]=0;         //-+
+  for (int i = 0; i < N; i++) //-Preset results
+    results[i] = 0;       //-+
 }
 
 void loop()
@@ -62,32 +62,32 @@ void loop()
   unsigned int d;
 
   int counter = 0;
-  for(unsigned int d=0;d<N;d++)
+  for (unsigned int d = 0; d < N; d++)
   {
-    int v=analogRead(0);    //-Read response signal
-    CLR(TCCR1B,0);          //-Stop generator
-    TCNT1=0;                //-Reload new frequency
-    ICR1=d;                 // |
-    OCR1A=d/2;              //-+
-    SET(TCCR1B,0);          //-Restart generator
+    int v = analogRead(0);  //-Read response signal
+    CLR(TCCR1B, 0);         //-Stop generator
+    TCNT1 = 0;              //-Reload new frequency
+    ICR1 = d;               // |
+    OCR1A = d / 2;          //-+
+    SET(TCCR1B, 0);         //-Restart generator
 
-    results[d]=results[d]*0.5+(float)(v)*0.5; //Filter results
-   
+    results[d] = results[d] * 0.5 + (float)(v) * 0.5; //Filter results
+
     freq[d] = d;
 
- //   plot(v,0);              //-Display
- //   plot(results[d],1);
-  // delayMicroseconds(1);
+    //   plot(v,0);              //-Display
+    //   plot(results[d],1);
+    // delayMicroseconds(1);
   }
 
 
-PlottArray(1,freq,results); 
- 
+  PlottArray(1, freq, results);
 
-  TOG(PORTB,0);            //-Toggle pin 8 after each sweep (good for scope)
+
+  TOG(PORTB, 0);           //-Toggle pin 8 after each sweep (good for scope)
 }
-   
 
-   
-    
- 
+
+
+
+
