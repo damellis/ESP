07b0acc74f1139cafb5d719df13ca4b0ad906bc8
David A. Mellis <dam@mellis.org>
2016-08-10 14:38:06 -0700

    Switch color sensor tuneables to callbacks.
    So they don’t cause the whole setup() function to be re-run.

--- Xcode/ESP/src/examples/user_color_sensor.cpp
+++ Xcode/ESP/src/examples/user_color_sensor.cpp
@@ -24,6 +24,39 @@ double null_rej = 5.0;
 bool send_repeated_predictions = false;
 int timeout = 100;
 
+void updateAlwaysPickSomething(bool new_val) {
+    pipeline.getClassifier()->enableNullRejection(!new_val);
+}
+
+void updateVariability(double new_val) {
+    pipeline.getClassifier()->setNullRejectionCoeff(new_val);
+    pipeline.getClassifier()->recomputeNullRejectionThresholds();
+}
+
+void updateSendRepeatedPredictions(bool new_val) {
+    if (new_val) {
+        assert(pipeline.getNumPostProcessingModules() == 0);
+        pipeline.addPostProcessingModule(ClassLabelTimeoutFilter(timeout));
+    } else {
+        assert(pipeline.getNumPostProcessingModules() == 1);
+        pipeline.removePostProcessingModule(0);
+    }
+}
+
+void updateTimeout(int new_val) {
+    if (pipeline.getNumPostProcessingModules() > 0) {
+        ClassLabelTimeoutFilter *filter =
+            dynamic_cast<ClassLabelTimeoutFilter *>
+                (pipeline.getPostProcessingModule(0));
+        assert(filter != nullptr);
+        filter->setTimeoutDuration(new_val);
+    }
+    
+    // else do nothing, i.e. allow the user to adjust the timout parameter
+    // even if it's not currently being used (because send_repeated_predictions
+    // is false).
+}
+
 void setup() {
     stream.useNormalizer(normalize);
     stream.setLabelsForAllDimensions({"red", "green", "blue"});
@@ -47,18 +80,18 @@ void setup() {
     registerTuneable(always_pick_something, "Always Pick Something",
         "Whether to always pick (predict) one of the classes of training data, "
         "even if it's not a very good match. If selected, 'Color Variability' "
-        "will not be used.");
+        "will not be used.", updateAlwaysPickSomething);
     registerTuneable(null_rej, 1.0, 25.0, "Color Variability",
          "How different from the training data a new color reading can be and "
          "still be considered the same color. The higher the number, the more "
-         "different it can be.");
+         "different it can be.", updateVariability);
     registerTuneable(send_repeated_predictions, "Send Repeated Predictions",
         "Whether to send repeated predictions while a pose is being held. If "
         "not selected, predictions will only be sent on transition from one "
-        "pose to another.");
+        "pose to another.", updateSendRepeatedPredictions);
     registerTuneable(timeout, 1, 3000,
         "Timeout",
         "How long (in milliseconds) to wait after recognizing a class before "
         "recognizing a different one. Only used if 'Send Repeated Predictions' "
-        "is selected.");
+        "is selected.", updateTimeout);
 }
