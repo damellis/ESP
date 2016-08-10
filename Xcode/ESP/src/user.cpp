5532a8397ac68685f4e2fa1abe2ee41e31057f0b
Ben Zhang <benzh@cs.berkeley.edu>
2016-08-10 12:00:16 -0700

    move user_audio to user_speaker

--- Xcode/ESP/src/user.cpp
+++ Xcode/ESP/src/user.cpp
@@ -2,7 +2,6 @@
 
 // This is a hack.
 // Otherwise, we will need to create a target for each user application.
-#include "examples/user_audio.cpp"
 //#include "examples/user_audio_beat_detection.cpp"
 //#include "examples/user_color_sensor.cpp"
 //#include "examples/user_accelerometer_gestures.cpp"
@@ -10,5 +9,6 @@
 //#include "examples/user_accelerometer_walk_detection.cpp"
 //#include "examples/user_capacitive_sensing.cpp"
 //#include "examples/user_speech_detection.cpp"
+#include "examples/user_speaker.cpp"
 //#include "examples/user_sudden_motion.cpp"
 //#include "examples/user_touche.cpp"
